2aa2cc188f48a09cc920f814d05050be5a75cd85
Devesh <rakhejadevesh@gmail.com>
2023-08-07 20:15:18 +0530

    files changed

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
-require('dotenv').config();  //Load Env
-const express = require('express');
+//require('dotenv').config();  //Load Env
+const express = require("express");
 
-const db = require('./config/mongoose');
-const port = process.env.PORT || 8000;
+const db = require("./config/mongoose");
+//const port = process.env.PORT || 8000;
+const port = 8000;
 const app = express();
-const path = require('path');
-const expressLayouts = require('express-ejs-layouts');
+const path = require("path");
+const expressLayouts = require("express-ejs-layouts");
 
 app.use(express.urlencoded());
-app.use(express.static('assets'));
+app.use(express.static("assets"));
 app.use(expressLayouts);
 
 // extract style and scripts from sub pages into the layout
-app.set('layout extractStyles', true);
-app.set('layout extractScripts', true);
+app.set("layout extractStyles", true);
+app.set("layout extractScripts", true);
 
 // set up the view engine
-app.set('view engine', 'ejs');
-app.set('views', './views');
+app.set("view engine", "ejs");
+app.set("views", "./views");
 
 // use express router
-app.use('/', require('./routes'));
+app.use("/", require("./routes"));
 app.listen(port, function (err) {
   if (err) {
     console.log(`Error in running the server: ${err}`);
